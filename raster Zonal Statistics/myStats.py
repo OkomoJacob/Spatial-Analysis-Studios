1d0a89bebf45136603d69e4bc9e6d1c2ffc606c8
okomojacob <jaylabs2030@gmail.com>
2021-02-16 13:12:17 +0300

    My step by step codes

---
+++ raster Zonal Statistics/myStats.py
@@ -0,0 +1,15 @@
+from geopandas import plotting as plot
+from matplotlib import pyplot as plt
+from rasterio.plot import show
+import geopandas as gpd
+import rasterstats
+import rasterio
+
+# Read the distrct .shp
+districts = gpd.read_file(r'Districts/districts.shp')
+districts.plot()
+rainFall = rasterio.open('Rainfall Data Rasters/2020-4-3.tif', mode='r')
+# show(rainFall)
+
+# Plot both raster and vector at ago using matplotlib
+fig, ax = plt.subplots(1, 1)
